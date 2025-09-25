274f09b449cd202ebf263eac17afd0c91d58d29b
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-24 20:44:51 -0700

    Commit, added input file

--- code.cpp
+++ code.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-
 using namespace std;
+
+class Movie {
+private:
+    string title;
+    string screenWriter;
+    int yearReleased;
+
+public:
+    string getTitle() {return title;}
+    string getScreenWriter() {return screenWriter;}
+    int getYear() {return yearReleased;}
+
+    void setTitle(string t) {title = t;}
+    void setScreenWriter(string sw) {screenWriter = sw;}
+    void setYear(int y) {yearReleased = y;}
+
+    void print() {
+        cout << "Title: " << title << endl;
+        cout << "Screen Writer: " << screenWriter << endl;
+        cout << "Year Released: " << yearReleased << endl;
+    }
+};
+
 
 int main() {
     
